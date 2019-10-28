6999d7063c637203dd4fac488311d7d06463dba9
Gerardo Naranjo <a01209499@itesm.mx>
2019-10-27 20:25:57 -0600

    Ejemplos de PHP hechos en clase

---
+++ ExamplePHP/functions.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>functions</title>
+  </head>
+
+  <body>
+    <h1>functions</h1>
+  </body>
+</html>
